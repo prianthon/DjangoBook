fa46a21676af4323a0dbc0b58de232912ea8ca3d
Pri Anton Subardio <prianthon@MacBook-Pro-Pri.local>
2016-11-20 19:31:14 +0700

    timeurl

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -14,9 +14,10 @@ Including another URLconf
 """
 from django.conf.urls import include, url
 from django.contrib import admin
-from mysite.views import hello
+from mysite.views import hello, current_datetime
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
     url(r'^hello/$', hello),
+    url(r'^time/$', current_datetime),
 ]
