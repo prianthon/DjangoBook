c8d3f2d8f123d8310a211e32705d70ddd9d17c41
Pri Anton Subardio <prianthon@MacBook-Pro-Pri.local>
2016-11-29 09:29:40 +0700

    admin author

--- mysite/books/admin.py
+++ mysite/books/admin.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
 from .models import Publisher, Author, Book
 
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ('first_name', 'last_name', 'email')
+
 # Register your models here.
 admin.site.register(Publisher)
-admin.site.register(Author)
+admin.site.register(Author, AuthorAdmin)
 admin.site.register(Book)
