9008cbc538238d5be9202efe5b199a4064cc5d18
Pri Anton Subardio <prianthon@MacBook-Pro-Pri.local>
2016-11-29 20:18:02 +0700

    search complete

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -15,10 +15,13 @@ Including another URLconf
 from django.conf.urls import include, url
 from django.contrib import admin
 from mysite.views import hello, current_datetime, hours_ahead
+from books import views
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
     url(r'^hello/$', hello),
     url(r'^time/$', current_datetime),
     url(r'^time/plus/(\d{1,2})/$', hours_ahead),
+    url(r'^search-form/$', views.search_form),
+    url(r'^search/$', views.search),
 ]
