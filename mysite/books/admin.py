a984dc1a0cb0a2058a3fa913428ab4a712367434
Pri Anton Subardio <prianthon@MacBook-Pro-Pri.local>
2016-11-29 14:21:44 +0700

    publisher id

--- mysite/books/admin.py
+++ mysite/books/admin.py
@@ -10,6 +10,8 @@ class BookAdmin(admin.ModelAdmin):
     list_filter = ('publication_date',)
     date_hierarchy = 'publication_date'
     ordering = ('-publication_date',)
+    filter_horizontal = ('authors',)
+    raw_id_fields = ('publisher',)
 
 # Register your models here.
 admin.site.register(Publisher)
